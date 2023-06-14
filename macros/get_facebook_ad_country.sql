a09fa4b1514c0cbbf2b4c6b7c4d79c49f1fa0af2
mohammad.mreish <svc_dna@stage-entertainment.com>
2023-06-14 08:59:55 +0200

    facebook france

--- macros/get_facebook_ad_country.sql
+++ macros/get_facebook_ad_country.sql
@@ -5,6 +5,9 @@
    when 2441671829467106 then 'Germany'
    when 135351755395794 then 'France'
    when 316290149907771 then 'France'
+   when 1406882756790460 then 'France'
+   when 489261026747377 then 'France'
+   when 1025814388809084 then 'France'
    when 39334103 then 'Spain'
    when 3315948171865020 then 'Netherlands'
    when 377966129965129 then 'Netherlands'
@@ -17,4 +20,3 @@
 
 
 
- 
